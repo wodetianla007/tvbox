c91caee60cda50bc4fc023a3a72cb21afa8cf748
wodetianla007 <160271386+wodetianla007@users.noreply.github.com>
2025-11-27 14:24:34 +0800

    Create test.js

---
+++ xiaosa/test.js
@@ -0,0 +1,18 @@
+function init(ext) {
+    return {
+        sites: [
+            {
+                key: "csp_Bili",
+                name: "测试站点",
+                type: 3,
+                api: "csp_Bili"
+            }
+        ],
+        lives: [],
+        parses: [],
+        rules: [],
+        flags: [],
+        wallpaper: "",
+        disabled_wallpaper: ""
+    };
+}
